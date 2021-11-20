0eb209e3de42cf9d481aad893c203d2491aa8320
foont <foontinz@protonmail.com>
2021-11-20 10:27:49 +0300

    lsn1, task 3 done lsn2, task 1 done lsn2, task 2 done lsn2, task 3 done

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 if __name__ == '__main__':
-    #Write a program, which has two print statements to print the following text (capital letters “O” and “H”, made from “#” symbols):
+    # Write a program, which has two print statements to print the following text (capital letters “O” and “H”, made from “#” symbols):
     print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
     print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
 
-
-    #Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
-    #“Good day <name>! <day> is a perfect day to learn some python.”
-
+    # Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+    # “Good day <name>! <day> is a perfect day to learn some python.”
     name = 'Dima'
     day = 'Saturday'
     print(f'Good day {name}!{day} is a perfect day to learn some python')
-    #print('Good day {}!{} is a perfect day to learn some python'.format(name,day))
+    # print('Good day {}!{} is a perfect day to learn some python'.format(name,day))
 
-    #Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
-
+    # Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
     surname = 'Yurinoff'
     print(surname + ' ' + name)
 
-
-
-
+    # Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+    a = 12
+    b = 4
+    print(f'{a+b}\n{a-b}\n{a/b}\n{a*b}\n{a**b}\n{a%b}\n{a//b}')
