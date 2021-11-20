5ad432284c1147a82e94f142534a7a6f9700d186
foont <foontinz@protonmail.com>
2021-11-20 10:12:32 +0300

    lsn1, task 3 done lsn2, task 1 done

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ if __name__ == '__main__':
     #Write a program, which has two print statements to print the following text (capital letters “O” and “H”, made from “#” symbols):
     print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
     print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
+
+
+    #Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+    #“Good day <name>! <day> is a perfect day to learn some python.”
+
+    name = 'Dima'
+    day = 'Saturday'
+    print(f'Good day {name}!{day} is a perfect day to learn some python')
