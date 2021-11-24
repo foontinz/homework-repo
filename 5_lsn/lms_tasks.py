84782c10dd3fbeeff15c904a55eecb3809fd412a
Foontin <foontinz@protonmail.com>
2021-11-24 14:15:57 +0300

    5_lsn tasks

---
+++ 5_lsn/lms_tasks.py
@@ -0,0 +1,23 @@
+import random
+
+if __name__ == '__main__':
+
+
+
+    #The Guessing Game.
+
+    number = str(random.randint(1, 10))
+    guessed = 0
+    while (number != guessed):
+        guessed = input('Enter number between 1 and 10')
+        if number == guessed:
+            print('You`ve won')
+            break
+        else:
+            print('Nope -_-')
+
+    #The birthday greeting program.
+
+    name = input('Enter your name')
+    age = int(input('Enter your age'))
+    print(f"Hello {name}, on your next birthday you’ll be {age+1} years")
