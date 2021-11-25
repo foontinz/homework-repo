328138c69c72c643c0c433a754935dcd50b23896
Foontin <foontinz@protonmail.com>
2021-11-25 19:55:54 +0300

    lms

--- lsn_5_tasks/lms_tasks_3_4.py
+++ lsn_5_tasks/lms_tasks_3_4.py
@@ -22,3 +22,40 @@ if __name__ == '__main__':
             editing_word = editing_word[:index] + editing_word[index + 1:]
             # print(editing_word, 'editing word')
         print(new_word)
+
+        # Используя модуль random и его функции randint напишите игру "математика 5кл".
+
+    while True:
+        equation_rand = random.randint(1, 3)
+        x = random.randint(0, 30)
+        equation_1 = 'y = 2x + 3'
+        equation_2 = 'y = 3x + 15'
+        equation_3 = 'y = x + 7'
+        equation_1_solution = str(2 * x + 3)
+        equation_2_solution = str(3 * x + 15)
+        equation_3_solution = str(x + 7)
+        if equation_rand == 1:
+            print(f'{equation_1} \nx = {x}')
+            ans = input('y = ')
+            if ans == equation_1_solution:
+                print('Перемога')
+                break
+            else:
+                print('Зрада')
+
+        elif equation_rand == 2:
+            print(f'{equation_2} \nx = {x}')
+            ans = input('y =')
+            if ans == equation_2_solution:
+                print('Перемога')
+                break
+            else:
+                print('Зрада')
+        else:
+            print(f'{equation_3} \nx = {x}')
+            ans = input('y =')
+            if ans == equation_3_solution:
+                print('Перемога')
+                break
+            else:
+                print('Зрада')
